95e02769687e0c4bf6b1868f25ee2f796a155a88
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-09-08 14:43:34 +0200

    chore(create-property):implent create property

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/index';
 import Dashboard from './pages/dashboard';
 import Ads from './pages/all_ads';
 import Ad from './pages/specific_ad';
+import CreateAd from './pages/createAd';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/dashboard" component={Dashboard}/>
             <Route path="/ads" component={Ads}/>
             <Route path="/ad" component={Ad}/>
+            <Route path="/Create-ad" component={CreateAd}/>
           </Switch>
           <Footer/>
       </div>
